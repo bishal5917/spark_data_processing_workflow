2a8f40c2620438a48b5190b999153f58889aa7a1
bishal5917 <poudelb172@gmail.com>
2025-01-11 10:56:49 +0545

    loading rdd from external datasets

---
+++ src/test/java/create_rdd/CreateRddUsingJavaParallelizeTest.java
@@ -0,0 +1,62 @@
+package create_rdd;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CreateRddUsingJavaParallelizeTest {
+
+    private static SparkConf sparkConf;
+    private static JavaSparkContext sparkContext;
+
+    @BeforeAll
+    static void beforeAll(){
+       sparkConf = new SparkConf()
+                .setAppName("CreateRDDUsingParallelizeTest")
+                .setMaster("local[*]");
+       sparkContext = new JavaSparkContext(sparkConf);
+    }
+
+    @Test
+    @DisplayName("Create an empty RDD with no partitions in spark")
+    void createEmptyRddWithNoPartitionInSpark(){
+        try{
+            final var emptyRdd = sparkContext.emptyRDD();
+            System.out.println(emptyRdd);
+            System.out.println("Number of partitions " + emptyRdd.getNumPartitions());
+        }catch (Exception ignored){
+        }
+    }
+
+    @Test
+    @DisplayName("Create an empty RDD with default partitions in spark")
+    void createEmptyRddWithDefaultPartitionInSpark(){
+        try{
+            final var emptyRdd = sparkContext.parallelize(List.of());
+            System.out.println(emptyRdd);
+            System.out.println("Number of partitions " + emptyRdd.getNumPartitions());
+        }catch (Exception ignored){
+        }
+    }
+
+    @Test
+    @DisplayName("Create an empty RDD with default partitions in spark")
+    void createSparkRddWithDefaultPartitionInSpark(){
+        try{
+            // create the list
+            List<Integer> dataInRdd = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8));
+            final var myRdd = sparkContext.parallelize(dataInRdd);
+            System.out.println(myRdd);
+            System.out.println("Number of partitions: " + myRdd.getNumPartitions());
+            System.out.println("Total Count: " + myRdd.count());
+            myRdd.collect().forEach(System.out::println);
+        }catch (Exception ignored){
+        }
+    }
+}
---
+++ src/test/java/create_rdd/RddUsingExternalDatasetsTest.java
@@ -0,0 +1,53 @@
+package create_rdd;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.nio.file.Path;
+
+public class RddUsingExternalDatasetsTest {
+
+    private static SparkConf sparkConf;
+    private static JavaSparkContext sparkContext;
+
+    @BeforeAll
+    static void beforeAll(){
+        sparkConf = new SparkConf()
+                .setAppName("CreateRDDUsingParallelizeTest")
+                .setMaster("local[*]");
+        sparkContext = new JavaSparkContext(sparkConf);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings =
+            {"src\\test\\resources\\1000_random_words.txt"})
+    @DisplayName("Test loading local test files into spark RDD")
+    void testLoadingLocalTextFilesIntoSparkRDD(final String localFilePath){
+        try{
+            final var rdd = sparkContext.textFile(localFilePath);
+            System.out.println("Total lines in the file " + rdd.count());
+            rdd.take(10).forEach(System.out::println);
+        }catch (Exception ex){
+            System.out.println("Exception: " + ex.getMessage());
+        }
+    }
+
+    @Test
+    @DisplayName("Loading local csv file into spark RDD")
+    void testLoadingLocalCSVFileIntoSparkRdd(){
+        try{
+            final var rdd = sparkContext.textFile("src/test/resources/country_capital.csv");
+            System.out.println("Total lines in the file " + rdd.count());
+            System.out.println(rdd.first());
+            rdd.take(10).forEach(System.out::println);
+        }catch (Exception ex){
+            System.out.println("Exception: " + ex.getMessage());
+        }
+    }
+
+}
